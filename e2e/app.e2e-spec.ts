c6f283a978b6068ab928ebe701b1daef95631781
Pascal Temel <admin@temel.me>
2016-09-14 11:25:28 +0200

    tabify

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AngularMaterial2SeedPage } from './app.po';
 
 describe('angular-material2-seed App', function() {
-  let page: AngularMaterial2SeedPage;
+	let page: AngularMaterial2SeedPage;
 
-  beforeEach(() => {
-    page = new AngularMaterial2SeedPage();
-  });
+	beforeEach(() => {
+		page = new AngularMaterial2SeedPage();
+	});
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+	it('should display message saying app works', () => {
+		page.navigateTo();
+		expect(page.getParagraphText()).toEqual('app works!');
+	});
 });
