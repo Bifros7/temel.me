e4f7efbc4e2765e91e4217b3f9489de4c6fa1999
Pascal Temel <admin@temel.me>
2016-09-24 00:55:53 +0200

    working navigation prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,40 @@ import { NavigationitemModel } from './navigationitem/navigationitem.model';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	private _child: NavigationitemModel = new NavigationitemModel(
+	private _child1: NavigationitemModel = new NavigationitemModel(
 		[],
-		'Child',
+		'Child 1',
 		'child_care',
+		'/'
+	)
+	private _child2: NavigationitemModel = new NavigationitemModel(
+		[],
+		'Child 2',
+		'donut_small',
 		'/stadtanzeiger'
 	)
+	private _child3: NavigationitemModel = new NavigationitemModel(
+		[],
+		'Child 3',
+		'event',
+		'/home'
+	)
+	private _childNested: NavigationitemModel = new NavigationitemModel(
+		[this._child1, this._child2, this._child3,],
+		'Nested child',
+		'view_list',
+		'/stadtanzeiger'
+	)
+
 	private _views: Array<NavigationitemModel> = [
 		new NavigationitemModel(
-			[this._child,this._child,this._child,this._child,this._child,this._child,],
+			[this._child1, this._child2, this._childNested, this._child3,],
 			'CRM',
 			'account_circle',
 			'',
 		),
 		new NavigationitemModel(
-			[this._child,this._child,this._child,this._child,this._child,this._child,],
+			[this._child1, this._child2, this._childNested, this._child3,],
 			'ERP',
 			'monetization_on',
 			'',
--- src/app/navigationitem/navigationitem.component.ts
+++ src/app/navigationitem/navigationitem.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { animate, Component, EventEmitter, Input, Output, state, style, transition, trigger } from '@angular/core';
 
 import { NavigationitemModel } from './navigationitem.model';
 
 @Component({
 	selector: 'navigationitem',
 	templateUrl: './navigationitem.component.html',
-	styleUrls: ['./navigationitem.component.css']
+	styleUrls: ['./navigationitem.component.css'],
+	animations: [
+		trigger('rotateArrow', [
+			state('true' , style({ transform: 'rotate(180deg)' })),
+			state('false', style({ transform: 'rotate(0deg)' })),
+			transition('* => *', animate('300ms')),
+		]),
+		trigger('expandChildren', [
+			state('true', style({ transform: 'translateX(0)' })),
+			transition('void => *', [
+				style({ transform: 'translateX(-100%)' }),
+				animate('200ms')
+			]),
+		])
+	]
 })
 export class NavigationitemComponent {
 	@Input() public navigationitem: NavigationitemModel;
+	@Output() public linkClicked: EventEmitter<void> = new EventEmitter<void>();
 
 	private _childrenVisible: boolean = false;
 }
